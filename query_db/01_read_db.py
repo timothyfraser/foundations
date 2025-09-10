d5a06dde7a3ef697ab59077e86766eac90f41ab5
Swathi Murali(sm3223) <swathimshetty19@gmail.com>
2025-09-09 20:04:57 -0400

    edit read_db.py

--- query_db/01_read_db.py
+++ query_db/01_read_db.py
@@ -6,6 +6,8 @@
 # !pip install pandas
 # Import pandas
 import pandas as pd
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Get download link - note: this must be specifically formatted as /export?format=csv; it is DIFFERENT from a share link. It also must be publicly viewable.
 path = "http://docs.google.com/spreadsheets/d/1k_4SZ-6achILnMD-tHU1PdrM7ppQgizJtyV4gf0b1wk/export?format=csv&gid=317348432"
