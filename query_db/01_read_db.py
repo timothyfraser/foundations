65ab3e296e40cc7a8f123f9bf270c167f50b9a9a
Swathi Murali(sm3223) <swathimshetty19@gmail.com>
2025-09-12 12:31:04 -0400

    add python3 relevant comments

--- query_db/01_read_db.py
+++ query_db/01_read_db.py
@@ -2,12 +2,17 @@
 
 # Python script to read in a PUBLIC, VIEWABLE Google Sheet
 
+# ******************* You can run python from the terminal by typing: python3 query_db/01_read_db.py or python query_db/01_read_db.py *******************
+# ******************* based on the python version you have installed on your system. Use this command to check your python version: python --version
+
 # ✅ Recommended Setup
 # !pip install pandas
 # Import pandas
 import pandas as pd
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+
+#************* Uncomment below two lines to ignore SSL CERTIFICATE ERRORS WHILE USING PYTHON3 *************
+#import ssl
+#ssl._create_default_https_context = ssl._create_unverified_context
 
 # Get download link - note: this must be specifically formatted as /export?format=csv; it is DIFFERENT from a share link. It also must be publicly viewable.
 path = "http://docs.google.com/spreadsheets/d/1k_4SZ-6achILnMD-tHU1PdrM7ppQgizJtyV4gf0b1wk/export?format=csv&gid=317348432"
